fe5b994d21cd5447699c21b9461d1fc585323dc7
rosicustodio <rosimarcustodio@gmail.com>
2021-05-27 02:33:10 -0300

    desafio 04

---
+++ script.js
@@ -0,0 +1,25 @@
+function frasedodia(){
+    console.log("Floresça onde estiver")
+}
+frasedodia ()
+
+
+function nome(meunome){
+    console.log(`meu nome é ${meunome}`)
+}
+nome(`Rosimar`)
+
+
+function dados1(nome,idade,estilomusical){
+    console.log(`${nome},${idade},${estilomusical}`)
+}
+dados1 (`Rosimar`, 47,`sou eclética para estilo musical`)
+
+function dados2(filmefavorito,musicafavorita){
+    console.log(`${filmefavorito},${musicafavorita}`)
+}
+dados2(`A procura da felicidade`,`Stuck you`)
+
+
+
+
